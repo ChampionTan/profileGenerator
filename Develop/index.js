018be617133909af99db45ca8441c647b3ec03ba
unknown <tannerwlumsden@gmail.com>
2023-01-04 18:50:43 -0500

    fixed questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,18 +5,29 @@ const Intern = require('./lib/Intern');
 const generateHtml = require('./src/page-template.js');
 const fs = require('fs');
 
-const managerInfo = () => {
-	return inquirer
-	.prompt([
-		{
+const memberArray = [];
+
+const teamInfo = [
+	
+	{
+		type: 'list',
+		name: 'position',
+		message: 'What position is this team member?',
+		choices: ['Manager', 'Engineer', 'Intern']
+	},	
+	
+	
+	
+	
+	{
 			type: 'input',
 			name: 'name',
-			message: 'What is the Managers name?',
+			message: 'What is the team members name?',
 			validate: input => {
 				if (input) {
 					return true;
 				}else {
-					console.log('Please input a managers name');
+					console.log('Please input a team member name');
 					return false;
 				}
 			}
@@ -24,12 +35,12 @@ const managerInfo = () => {
 		{
 			type: 'input',
 			name: 'email',
-			message: 'What is the Managers email?',
+			message: 'What is the team members email?',
 			validate: input => {
 				if (input) {
 					return true;
 				}else {
-					console.log('Please input managers email');
+					console.log('Please input team members email');
 					return false;
 				}
 			}
@@ -37,12 +48,26 @@ const managerInfo = () => {
 		{
 			type: 'input',
 			name: 'id',
-			message: 'What is the Managers ID?',
+			message: 'What is the team members ID?',
 			validate: input => {
 				if (input) {
 					return true;
 				}else {
-					console.log('Please input a managers ID');
+					console.log('Please input the team members ID');
+					return false;
+				}
+			}
+		},
+		{
+			type: 'input',
+			name: 'number',
+			message: 'What is the team members office number?',
+			when: (officeInput) => officeInput.role === 'Manager',
+			validate: officeInput => {
+				if (officeInput) {
+					return true;
+				}else {
+					console.log('Please input the team members office number');
 					return false;
 				}
 			}
@@ -50,12 +75,27 @@ const managerInfo = () => {
 		{
 			type: 'input',
 			name: 'github',
-			message: 'What is the Managers GitHub?',
-			validate: input => {
-				if (input) {
+			message: 'What is the team members GitHub?',
+			when: (githubInput) => githubInput.role === 'Engineer',
+			validate: githubInput => {
+				if (githubInput) {
 					return true;
 				}else {
-					console.log('Please input a managers GitHub');
+					console.log('Please input the team members GitHub');
+					return false;
+				}
+			}
+		},
+		{
+			type: 'input',
+			name: 'school',
+			message: 'What is the team members school?',
+			when: (schoolInput) => schoolInput.role === 'Intern',
+			validate: schoolInput => {
+				if (schoolInput) {
+					return true;
+				}else {
+					console.log('Please input the team members school');
 					return false;
 				}
 			}
